6684ca83701979260359d1b53cd1002ed021c677
Carter Hinsley <carterhinsley@gmail.com>
2015-03-08 17:02:40 +0000

    (feature) Add StreamIO module

---
+++ src/StreamIO.hs
@@ -0,0 +1,9 @@
+{-|
+Module      : StreamIO
+Description : Parser input/output stream handling
+Copyright   : (c) chop-lang, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
+module StreamIO where
+
