dabd4ad13914aee3f9417bcb6b235a82f39e7ae3
Carter Hinsley <carterhinsley@gmail.com>
2015-03-08 21:06:11 +0000

    (refactor) Rename Parser module to EBNFParser

--- src/EBNFParser.hs
+++ src/EBNFParser.hs
@@ -1,9 +1,9 @@
 {-|
-Module      : Parser
+Module      : EBNFParser
 Description : EBNF-parsing utilities
 Copyright   : (c) chop-lang, 2015
 License     : MIT
 Maintainer  : carterhinsley@gmail.com
 -}
-module Parser where
+module EBNFParser where
 
