1b8ecc8d40230396fc554e6949e844d1f747ed5f
Carter Hinsley <carterhinsley@gmail.com>
2015-03-08 22:47:12 +0000

    (feature) Add EBNF-parsing AST datatypes

--- src/EBNFParser.hs
+++ src/EBNFParser.hs
@@ -7,3 +7,24 @@ Maintainer  : carterhinsley@gmail.com
 -}
 module EBNFParser where
 
+data ContainerType = Group
+                   | Option
+                   | Repetition
+                   deriving (Show)
+
+data Terminal = TermAlt
+              | TermComma
+              | TermEnd
+              | TermEquals
+              | TermExclude
+              | TermIdentifier Text
+              | TermSpecial Text
+              | TermString Text
+              deriving (Show)
+
+data Token = TContainer ContainerType Token
+           | TTerminal Terminal
+           deriving (Show)
+
+data AST = AST [Token] deriving (Show)
+
