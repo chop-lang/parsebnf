d3933d0732573ed1275b27b8e9308af99dbaa9e0
Carter Hinsley <carterhinsley@gmail.com>
2015-03-09 14:04:51 +0000

    (nocompile|cleanup|refactor) Change EBNFParser.parse to use a 'case' expression

--- src/EBNFParser.hs
+++ src/EBNFParser.hs
@@ -34,28 +34,20 @@ type AST = [Token]
 
 parse :: String -> AST
 parse ebnf@(x:xs)
-    | x == '|'              = TTerminal (TermAlt)
-                            : parse xs
-    | x == ','              = TTerminal (TermComma)
-                            : parse xs
-    | x == ';' || x == '.'  = TTerminal (TermEnd)
-                            : parse xs
-    | x == '='              = TTerminal (TermEquals)
-                            : parse xs
-    | x == '-'              = TTerminal (TermExclude)
-                            : parse xs
-    | isAlpha x             = TTerminal (TermIdentifier {- ADD -})
-                            : {- ADD -}
-    | x == '?'              = TTerminal (TermSpecial {- ADD -})
-                            : {- ADD -}
-    | x == '"' || x == '\'' = TTerminal (TermString {- ADD -})
-                            : {- ADD -}
-    | x == '('              = if head xs == '*'
-                                  then parse {- ADD -}
-                                  else TContainer Group {- ADD -}
-    | x == '['              = TContainer Option {- ADD -}
-    | x == '{'              = TContainer Repetition {- ADD -}
-    | isSpace x             = parse xs
-    | otherwise             = error $ "Invalid character '" ++ [x] ++ "'."
+    | isSpace x      = parse xs
+    | isAlpha x      = TTerminal (TermIdentifier {- ADD -}) : {- ADD -}
+    | x `elem` "\"'" = TTerminal (TermString {- ADD -}) : {- ADD -}
+    | x `elem` ";."  = TTerminal (TermEnd) : parse xs
+    | otherwise = case x of '|' -> TTerminal TermAlt     : parse xs
+                            ',' -> TTerminal TermComma   : parse xs
+                            '=' -> TTerminal TermEquals  : parse xs
+                            '-' -> TTerminal TermExclude : parse xs
+                            '?' -> TTerminal (TermSpecial {- ADD -}) : {- ADD -}
+                            '(' -> if head xs == '*'
+                                        then parse {- ADD -}
+                                        else TContainer Group {- ADD -}
+                            '[' -> TContainer Option {- ADD -}
+                            '{' -> TContainer Repetition {- ADD -}
+                            _   -> error $ "Invalid character '" ++ [x] ++ "'."
 parse [] = []
 
