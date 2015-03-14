290eb30ee00a8a2667a78703f6895fec9f723b64
Carter Hinsley <carterhinsley@gmail.com>
2015-03-14 23:43:15 +0000

    (feature) Add EBNFInterpreter module
    Prepare for development of functions and types in the EBNFInterpreter
    module; create the file for the module and add it to the cabal config
    file.

---
+++ src/EBNFInterpreter.hs
@@ -0,0 +1,14 @@
+{-|
+Module      : EBNFInterpreter
+Description : EBNF-AST-interpreting utilities
+Copyright   : (c) chop-lang, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
+module EBNFInterpreter where
+
+import EBNFParser ( ContainerType(..)
+                  , Terminal(..)
+                  , Token(..)
+                  , AST )
+
