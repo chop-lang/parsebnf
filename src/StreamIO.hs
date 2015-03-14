48655a455571595d8ec3c92a2c19ad0342ae8f21
Carter Hinsley <carterhinsley@gmail.com>
2015-03-14 23:55:44 +0000

    (refactor) Remove StreamIO module
    Functionality for StreamIO was placed into CLI.

--- src/StreamIO.hs
+++
@@ -1,9 +0,0 @@
-{-|
-Module      : StreamIO
-Description : Parser input/output stream handling
-Copyright   : (c) chop-lang, 2015
-License     : MIT
-Maintainer  : carterhinsley@gmail.com
--}
-module StreamIO where
-
