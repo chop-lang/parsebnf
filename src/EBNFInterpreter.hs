e96ccceb8aec217e3141020a885f091c2b10fc36
Carter Hinsley <carterhinsley@gmail.com>
2015-03-18 10:15:36 +0000

    (feature) Lay out EBNF IR data types
    The internal representation of an EBNF AST exists within Haskell
    data types. The IR can be constructed from an AST (defined as
    EBNFParser.AST), but functions for transcription still need to be
    composed.
    
    Other type classes like Read could probably be derived but I'm not
    particularly worried about adding such derivations right now,
    especially considering how trivial it would be to add such derivations.

--- src/EBNFInterpreter.hs
+++ src/EBNFInterpreter.hs
@@ -7,8 +7,27 @@ Maintainer  : carterhinsley@gmail.com
 -}
 module EBNFInterpreter where
 
+import Data.Text (Text(..))
 import EBNFParser ( ContainerType(..)
                   , Terminal(..)
                   , Token(..)
                   , AST )
+
+data FTerminal = FTermExclude
+               | FTermMultiple
+               | FTermNumber Text
+               | FTermIdentifier Text
+               | FTermSpecial Text
+               | FTermString Text
+               deriving (Show)
+
+data FToken = FTContainer ContainerType FormContent
+            | FTTerminal FTerminal
+            deriving (Show)
+
+type FAlternation = [FToken]
+
+type FormContent = [FAlternation]
+
+type IR = (Text, FormContent)
 
