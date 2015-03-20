eb598c75804e876d42735a1155a1c5d609a0af44
Carter Hinsley <carterhinsley@gmail.com>
2015-03-20 07:51:15 +0000

    (feature) Add IREvaluator module
    IREvaluator will contain functions for accepting a Text value and
    parsing it based on rules set forth by IRForms in an IR value,
    returning an AST comprising primitive data types.
    
    IREvaluator should be one of the last modules for the foreseeable
    future. After it has been fully implemented, parsebnf should be fully
    operational, despite minor bugs like the one regarding optional commas,
    described in commit 4c1cefb7edd92f86392d2c6e7be7f10abf531778.

---
+++ src/IREvaluator.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : IREvaluator
+Description : EBNF IR evaluation
+Copyright   : (c) chop-lang, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
+module IREvaluator where
+
+import Data.Text (Text(..))
+import IRGenerator ( IRTerminal(..)
+                   , IRToken(..)
+                   , IRAlternation
+                   , IRContent
+                   , IRForm
+                   , IR )
+
